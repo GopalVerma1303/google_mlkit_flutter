f61ced5425833d883c5af75e0a8d95200f3ed0db
Gopal Verma <vgopaldps@gmail.com>
2023-02-20 20:36:56 +0530

    added tabs

---
+++ lib/homepage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_flutter/NlpApis.dart';
+import 'package:google_mlkit_flutter/visionapis.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(" G O O G L E   M L   K I T"),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: const [
+              TabBar(
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.remove_red_eye, color: Colors.blue),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.translate, color: Colors.blue),
+                  )
+                ],
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    Container(
+                      child: visionapis(),
+                    ),
+                    Container(
+                      child: nlpapis(),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/visionapis.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class visionapis extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("VISION APIS PAGE")),
+    );
+  }
+}
